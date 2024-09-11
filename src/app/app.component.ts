18f14e3be2a609c72fbfdb35a3fb0712aeb54f8c
Daniel Khalil <sousadks@saude.sc.gov.br>
2024-09-11 11:24:54 -0300

    Iniciando os componentes do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit } from '@angular/core';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { SidebarComponent } from './template/sidebar/sidebar.component';
+import jQuery from 'jquery';
+import { HomeComponent } from './template/home/home.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [NavbarComponent, SidebarComponent, HomeComponent],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ClientesApplicationFront';
+
+  ngAfterViewInit(): void {
+    this.actionMenuButton();
+  }
+
+  actionMenuButton() {
+    // Use 'strict' mode for self-invoking function
+    'use strict';
+
+    // Add active state to sidebar nav links
+    const path = window.location.href; // 'href' property of the DOM element is the absolute path
+
+    $('#layoutSidenav_nav .sb-sidenav a.nav-link').each(function () {
+      // Use type assertion to tell TypeScript that 'this' is an HTMLAnchorElement
+      const link = this as HTMLAnchorElement;
+      if (link.href === path) {
+        $(link).addClass('active');
+      }
+    });
+
+    // Toggle the side navigation
+    $('#sidebarToggle').on('click', function (e) {
+      e.preventDefault();
+      $('body').toggleClass('sb-sidenav-toggled');
+    });
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule
+  ],
+  bootstrap: [],
+})
+export class AppModule { }

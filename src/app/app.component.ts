839d68f4fc25c72b42fc933076df0611050a1aaf
= <=>
2024-10-15 17:33:31 -0300

    Ajuste de layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { SidebarComponent } from './template/sidebar/sidebar.component';
+import jQuery from 'jquery';
+import { HomeComponent } from './template/home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ClientesApplicationFront';
+
+  ngAfterViewInit(): void {
+    this.actionMenuButton();
+  }
+
+  actionMenuButton() {
+    // Use 'strict' mode for self-invoking function
+    'use strict';
+
+    // Add active state to sidebar nav links
+    const path = window.location.href; // 'href' property of the DOM element is the absolute path
+
+    $('#layoutSidenav_nav .sb-sidenav a.nav-link').each(function () {
+      // Use type assertion to tell TypeScript that 'this' is an HTMLAnchorElement
+      const link = this as HTMLAnchorElement;
+      if (link.href === path) {
+        $(link).addClass('active');
+      }
+    });
+
+    // Toggle the side navigation
+    $('#sidebarToggle').on('click', function (e) {
+      e.preventDefault();
+      $('body').toggleClass('sb-sidenav-toggled');
+    });
+  }
 }

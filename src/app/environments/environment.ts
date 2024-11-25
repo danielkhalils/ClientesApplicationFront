a7f0110b3600ff46e0c430993cc77ad49d60545e
= <=>
2024-11-25 08:07:39 -0300

    Integrando Auth2 para autenticação na aplicação

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,7 @@
 import { ServicoPrestado } from "../shared/model/servicoPrestado";
 
 export const environment = {
+    apiURLBase: 'http://localhost:8080',
     cliente: 'http://localhost:8080/api/clientes',
     servicoPrestado: 'http://localhost:8080/api/servicos-prestados'
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
+import { Usuario } from '../../shared/model/usuario';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +13,15 @@ export class LoginComponent {
   password?: string;
   loginError?: boolean;
   cadastrando?: boolean;
+  mensagemSucesso?: string | null;
 
   constructor(
-    private router: Router
-  ) {}
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   public onSubmit() {
-    console.log(`Usuario: ${this.username}, Pass: ${this.password}`);
-
-    this.router.navigate(['/home']);
+    this.router.navigate(['/login']);
   }
 
   public prepararCadastro(event: { preventDefault: () => void }) {
@@ -27,7 +29,25 @@ export class LoginComponent {
     this.cadastrando = true;
   }
 
-  public cancelarCadastro() {
+  public cancelarCadastro(event: { preventDefault: () => void }) {
+    event.preventDefault();
     this.cadastrando = false;
+    this.mensagemSucesso = null;
+    this.loginError = false;
+  }
+
+  public cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.authService
+        .salvar(usuario)
+        .subscribe( response => {
+          this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
+          this.loginError = false;
+        }, error => {
+          this.loginError = true;
+          this.mensagemSucesso = null;
+        })
   }
 }

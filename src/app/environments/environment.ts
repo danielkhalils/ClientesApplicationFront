5b77f65d9195b2deec86a4f3a1247f31b92c4635
= <=>
2024-11-25 12:51:15 -0300

    Implementando a tela de login e tratamento de errors no front.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,5 +3,8 @@ import { ServicoPrestado } from "../shared/model/servicoPrestado";
 export const environment = {
     apiURLBase: 'http://localhost:8080',
     cliente: 'http://localhost:8080/api/clientes',
-    servicoPrestado: 'http://localhost:8080/api/servicos-prestados'
+    servicoPrestado: 'http://localhost:8080/api/servicos-prestados',
+    clientId: 'my-angular-app',
+    clientSecret: '@321',
+    obterTokenUrl: '/oauth/token'
 }
